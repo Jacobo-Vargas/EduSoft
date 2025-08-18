226e6f315a9895d61111cb3ebe5ff9e64f3c19ac
Jacobo Vargas García <jacobovargas493@gmail.com>
2025-08-18 17:08:50 -0500

    add structure of projects, it´s a mono repo, always down changes and have it  merge updated

---
+++ backend/src/test/java/com/uniquindio/edu/edusoft/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.edusoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
