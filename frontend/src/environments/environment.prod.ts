c3ae37529939fa1106f2fec5688bd4427dd93ab3
Jacobo Vargas García <jacobovargas493@gmail.com>
2025-09-01 23:39:38 -0500

    fix: frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,10 @@ export const environment = {
   apiUrl2: 'https://edusoft.ddns.net/api-public',
   urlServer: 'https://edusoft.ddns.net/api',
   urlLogout: 'https://edusoft.ddns.net/api/auth/logout',
+  urlSendCodeEmail: '/auth/sendCodeEmail',
+  urlRecoverPassword: '/auth/recover-password',
+  urlUpdatePassword: '/auth/updatePassword',
+  urlLogin: '/auth/login',
 
   roles: {
     admin: 'ROLE_ADMIN'
