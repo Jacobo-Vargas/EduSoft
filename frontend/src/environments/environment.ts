fa5ca3fcb8850dbd23114d829bed8eae415c2ba1
Jacobo Vargas García <jacobovargas493@gmail.com>
2025-09-01 23:32:26 -0500

    fix (cherry picked from commit c3ae37529939fa1106f2fec5688bd4427dd93ab3)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false,
   apiUrl: '/api',
   apiUrl2: '/api-public',
-  urlServer: 'http://localhost:8080/api',
-  urlLogout: 'https://localhost:8080/api/auth/logout',
   urlLogin: '/auth/login',
   urlSendCodeEmail: '/auth/sendCodeEmail',
   urlRecoverPassword: '/auth/recover-password',
   urlUpdatePassword: '/auth/updatePassword',
+  urlServer: 'http://localhost:8443/api',
+  urlLogout: 'https://localhost:8443/api/auth/logout',
 
 
   //Temporary roles subject to change
