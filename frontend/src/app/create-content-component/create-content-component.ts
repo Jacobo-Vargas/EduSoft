5c2bec8b6948600e300eb50a4732d0e4ec94726e
JUANCONTRERAS <juanj.contrerasm@uqvirtual.edu.co>
2025-09-06 20:05:07 -0500

    no message

---
+++ frontend/src/app/create-content-component/create-content-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-content-component',
+  imports: [],
+  templateUrl: './create-content-component.html',
+  styleUrl: './create-content-component.css'
+})
+export class CreateContentComponent {
+
+}
